7da820c492825e199b11ae2da3cfe4bcb4ed16a5
LiomarRenner <68114557+LiomarRenner@users.noreply.github.com>
2020-11-23 22:50:57 -0300

    RepoUpdates

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from './styles';
+
 
 const Repo: React.FC = () => {
-  return <div />;
+  return ( 
+  <Container>
+      <Breadcrumb>
+        <RepoIcon />
+
+        <Link className={'username'} to={`/${username}`}>
+          {username}
+        </Link>
+
+        <span>/</span>
+
+        <Link className={'reponame'} to={`/${username}/${reponame}`}>
+          {reponame}
+        </Link>
+      </Breadcrumb>
+
+      <p>Contains all of my codes</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>1</b>
+          <span>Stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={'https://github.com/LiomarRenner'}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+  </Container>
+  );
 }
 
 export default Repo;
--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -1,5 +1,20 @@
-import styled from 'styled-components';
-
+import styled, { css } from 'styled-components';
+import { RiBookMarkLine } from 'react-icons/ri';
+import { AiOutlineFork } from 'react-icons/ai';
+import { FaGithub } from 'react-icons/fa';
 export const Container = styled.div`
   
 `;
+export const Breadcrumb = styled.div``;
+
+export const RepoIcon = styled()``;
+
+export const Stats = styled.div``;
+
+export const StarIcon = styled()``;
+
+export const ForkIcon = styled()``;
+
+export const LinkButton = styled.div``;
+
+export const GithubIcon = styled()``;
