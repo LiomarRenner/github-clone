9daea3922b7d8ca46b871b1de81835f7c195ff2c
LiomarRenner <68114557+LiomarRenner@users.noreply.github.com>
2020-12-03 02:15:11 -0300

    Footer, Header, themes.ts

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Line, Githublogo } from './styles';
+
+const Footer: React.FC = () => {
+  return (
+  <Container>
+      <Line />
+      <Githublogo />
+  </Container>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { FaGithub } from 'react-icons/fa'
+
+export const Container = styled.div`
+  
+`;
+export const Line = styled.div`
+  
+`;
+export const Githublogo = styled(Fagithub)`
+  fill: var(--border);
+`;
