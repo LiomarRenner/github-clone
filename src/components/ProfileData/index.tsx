dc1689d59e0ccb34ea63b137adaaedc9c7ef7b43
LiomarRenner <68114557+LiomarRenner@users.noreply.github.com>
2020-12-16 01:00:42 -0300

    ProfileDataUpdates
    Most Syntax updates

--- src/components/ProfileData/index.tsx
+++ src/components/ProfileData/index.tsx
@@ -1,89 +1,91 @@
-// @ts-ignore
 import React from 'react';
 
-import { 
-    Container, 
-    Flex, 
-    Avatar, 
-    Row, 
-    PeopleIcon, 
-    Column, 
-    CompanyIcon, 
-    LocationIcon, 
-    EmailIcon, 
-    BlogIcon 
+import {
+  Container,
+  Flex,
+  Avatar,
+  Row,
+  PeopleIcon,
+  Column,
+  CompanyIcon,
+  LocationIcon,
+  EmailIcon,
+  BlogIcon,
 } from './styles';
 
 interface Props {
-    username: string;
-    name: string;
-    avatarUrl: String;
-    followers: number;
-    following: number;
-    company?: string;
-    location?: string;
-    email?: string;
-    blog?: string;
+  username: string;
+  name: string;
+  avatarUrl: string;
+  followers: number;
+  following: number;
+  company?: string;
+  location?: string;
+  email?: string;
+  blog?: string;
 }
+
 const ProfileData: React.FC<Props> = ({
-    username,
-    name,
-    avatarUrl,
-    followers,
-    following, 
-    company, 
-    location, 
-    email, 
-    blog,
+  username,
+  name,
+  avatarUrl,
+  followers,
+  following,
+  company,
+  location,
+  email,
+  blog,
 }) => {
   return (
     <Container>
-        <Flex>
-            <Avatar src={avatarUrl} alt={username} />
-            <div>
-                <h1>{name}</h1>
-                <h2>{username}</h2>
-            </div>
-        </Flex>
+      <Flex>
+        <Avatar src={avatarUrl} alt={username} />
 
-        <Row>
-            <li>
-                <PeopleIcon/>
-                <b>{followers}</b>
-                <span>followers</span>
-            </li>
-            <li>
-                <b>{following}</b>
-                <span>following</span>
-            </li>
-        </Row>
-        
-        <Column>
-            {company &&( 
-            <li>
-                <CompanyIcon />
-                <span>{company}</span>
-            </li> 
-            )}
-            {location &&( 
-            <li>
-                <LocationIcon />
-                <span>{company}</span>
-            </li> 
-            )}
-            {email &&( 
-            <li>
-                <EmailIcon />
-                <span>{company}</span>
-            </li> 
-            )}
-            {blog &&( 
-            <li>
-                <BlogIcon />
-                <span>{company}</span>
-            </li> 
-            )}
-        </Column>
+        <div>
+          <h1>{name}</h1>
+          <h2>{username}</h2>
+        </div>
+      </Flex>
+
+      <Row>
+        <li>
+          <PeopleIcon />
+          <b>{followers}</b>
+          <span>followers</span>
+          <span>·</span>
+        </li>
+        <li>
+          <b>{following}</b>
+          <span>following</span>
+        </li>
+      </Row>
+
+      <Column>
+        {company && (
+          <li>
+            <CompanyIcon />
+            <span>{company}</span>
+          </li>
+        )}
+        {location && (
+          <li>
+            <LocationIcon />
+            <span>{location}</span>
+          </li>
+        )}
+        {email && (
+          <li>
+            <EmailIcon />
+            <span>{email}</span>
+          </li>
+        )}
+        {blog && (
+          <li>
+            <BlogIcon />
+            <span>{blog}</span>
+          </li>
+        )}
+      </Column>
     </Container>
   );
 };
