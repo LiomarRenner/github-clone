eb1ee7aa107d273a9ec0c43c014163b283c235a6
LiomarRenner <68114557+LiomarRenner@users.noreply.github.com>
2020-12-16 00:54:10 -0300

    Update styles.ts

--- src/components/ProfileData/styles.ts
+++ src/components/ProfileData/styles.ts
@@ -1,104 +1,116 @@
- // eslint-disable-next-line 
-import { scryRenderedComponentsWithType } from "react-dom/test-utils";
-
 import styled, { css } from 'styled-components';
-import { RiGroupLine, RiBuilding4Line, RiMapPin2Line, RiMailLine, RiLinksLine} from 'react-icons/ri';
+import {
+  RiGroupLine,
+  RiBuilding4Line,
+  RiMapPin2Line,
+  RiMailLine,
+  RiLinksLine,
+} from 'react-icons/ri';
 
 export const Container = styled.div``;
 
 export const Flex = styled.div`
-    display:flex;
-    align-items:center;
+  display: flex;
+  align-items: center;
+
+  > div {
+    margin-left: 24px;
+
+    > h1 {
+      font-size: 26px;
+      line-height: 1.25;
+      color: var(--gray-dark);
+      font-weight: 600;
+    }
+    > h2 {
+      font-size: 20px;
+      color: var(--username);
+      font-weight: 300;
+    }
+  }
+
+  @media (min-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
 
     > div {
-        margin-left: 24px;
-        > h1 {
-            font-size: 26px;
-            line-height: 1.25;
-            color: var( --gray-dark);
-            font-weight: 600;
-        }
-        > h2 {
-            font-size: 20px;
-            color: var(--username);
-            font-weight:300;
-        } 
+      margin-left: 0;
+      margin-top: 16px;
     }
-    @media (min-width: 758px) {
-        flex-direction: column;
-        align-items: flex-start;
-
-        >div {
-            margin-left: 0;
-            margin-top: 16px;
-        }
-    }
+  }
 `;
 
 export const Avatar = styled.img`
-    width:16%;
-    border-radius:50%;
+  width: 16%;
+  border-radius: 50%;
 
-    @media (min-width:768px) {
-        width: 100%;
-        margin-top: -34px;
-    }
+  @media (min-width: 768px) {
+    width: 100%;
+    margin-top: -34px;
+  }
 `;
 
 export const Row = styled.ul`
-    display:flex;
-    align-items:center;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  margin: 20px 0;
 
-    > li {
-        display: flex;
-        align-items: center;
-        flex-wrap: wrap;
-        margin: 20px;
+  > li {
+    display: flex;
+    align-items: center;
 
-        > span {
-            font-size: 14px;
-            color: var(--gray);
-        }
-        > * {
-            margin-right: 5px;
-        }
+    > span {
+      font-size: 14px;
+      color: var(--gray);
     }
-`;
- // eslint-disable-next-line 
-const iconCss = css`
-    width: 16px;
-    height: 16px;
-    fill: var(--icon);
-    flex-shrink:0;
+    > * {
+      margin-right: 5px;
+    }
+  }
 `;
 
-export const PeopleIcon = styled(RiGroupLine)``;
+const iconCSS = css`
+  width: 16px;
+  height: 16px;
+  fill: var(--icon);
+  flex-shrink: 0;
+`;
+
+export const PeopleIcon = styled(RiGroupLine)`
+  ${iconCSS}
+`;
 
 export const Column = styled.ul`
-    li {
-        display:flex;
-        align-items:center;
-        font-size: 14px;
+  li {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+  }
+  li + li {
+    margin-top: 10px;
+  }
+  span {
+    margin-left: 5px;
 
-        li + li {
-            margin-top: 10px;
-
-        }
-        span {
-            margin-left: 5px;
-
-            overflow: hidden;
-            text-overflow:ellipsis;
-            white-space: nowrap;
-        }
-    }
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
 `;
 
-export const CompanyIcon = styled(RiBuilding4Line)``;
+export const CompanyIcon = styled(RiBuilding4Line)`
+  ${iconCSS}
+`;
 
-export const LocationIcon = styled(RiMapPin2Line)``;
+export const LocationIcon = styled(RiMapPin2Line)`
+  ${iconCSS}
+`;
 
-export const EmailIcon = styled(RiMailLine)``;
+export const EmailIcon = styled(RiMailLine)`
+  ${iconCSS}
+`;
 
-export const BlogIcon = styled(RiLinksLine)``;
-
+export const BlogIcon = styled(RiLinksLine)`
+  ${iconCSS}
+`;
